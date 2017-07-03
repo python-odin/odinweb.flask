06dc5995883c744655dee5fa7243520b8ae88da2
Tim Savage <tim@savage.company>
2017-07-03 23:22:34 +1000

    Built out more sample code

--- example/__init__.py
+++ example/__init__.py
@@ -1,7 +1,20 @@
+import odin
+
 from flask import Flask
 from odinweb.flask import api
 
+
 app = Flask(__name__)
+
+
+class Book(odin.Resource):
+    title = odin.StringField()
+    published = odin.DateField()
+    authors = odin.TypedListField(odin.StringField())
+
+
+class BookApi(api.ResourceApi):
+    resource = Book
 
 
 @app.route("/")
@@ -9,5 +22,5 @@ def hello():
     return "HelloWorld!"
 
 api.Api(
-    api.ApiVersion()
+    api.ApiVersion(BookApi())
 ).register(app)
