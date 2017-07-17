13021ada1d99938e8516dd1b32dad74c8a70c2f9
Tim Savage <tim@savage.company>
2017-07-17 18:45:26 +1000

    Updated example to align with simplified documentation

--- example/__init__.py
+++ example/__init__.py
@@ -12,35 +12,43 @@ class User(odin.Resource):
     role = odin.StringField(choices=('a', 'b', 'c'))
 
 
+class Group(odin.Resource):
+    id = odin.IntegerField()
+    name = odin.StringField()
+
+
 class UserApi(api.ResourceApi):
     resource = User
 
     @api.listing
-    @doc.operation(tags=['user'])
     def get_user_list(self, request, limit, offset):
         return [
             User(1, "tim"),
             User(2, "sara"),
         ], 2
 
-    @api.create
-    @doc.body_param(User)
-    @doc.operation(tags=['user'])
-    @doc.response(200, 'Return new user', User)
-    def create_user(self, request):
+    @api.create(resource=User)
+    def create_user(self, request, resource):
         user = self.get_resource(request)
         user.id = 3
         return user
 
     @api.detail
-    @doc.operation(tags=['user'])
     @doc.query_param('full', type=api.Type.Boolean)
-    @doc.response(200, 'Return requested user.', User)
     def get_user(self, request, resource_id):
         """
         Get a user object
         """
         return User(resource_id, "tim")
+
+
+class GroupApi(api.ResourceApi):
+    resource = Group
+
+    @api.listing
+    @doc.operation(tags=['user'])
+    def list_groups(self):
+        pass
 
 
 app = Flask(__name__)
@@ -52,6 +60,7 @@ app.register_blueprint(
                 UserApi(),
             ),
         ),
-        debug_enabled=True
-    )
+        debug_enabled=True,
+        url_prefix='/example'
+    ),
 )
